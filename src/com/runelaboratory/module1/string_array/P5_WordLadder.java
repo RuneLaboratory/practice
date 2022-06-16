01d2f1e6aa3230b9d516ffc9d6a1d4b5d5163ade
RuneYap <runelaboratory94@gmail.com>
2022-06-16 13:49:40 +0800

    StringArray P5. Given two words and a dictionary, find the shortest transformation sequence from start to end.
    add BFS solution.

--- src/com/runelaboratory/module1/string_array/P5_WordLadder.java
+++ src/com/runelaboratory/module1/string_array/P5_WordLadder.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class P5_WordLadder {
 
     /*
-    Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from start to end,
+    Given two words (start and end), and a dictionary, find the length of the shortest transformation sequence from start to end,
     such that only one letter can be changed at a time and each intermediate word must exist in the dictionary.
     For example, given:
 
@@ -23,43 +23,81 @@ public class P5_WordLadder {
         String end = "cog";
         Set<String> disc = new HashSet<>(Arrays.asList("hot", "dot", "dog", "hit", "lot", "log"));
         System.out.println("solution : " + solution(start, end, new HashSet<>(disc)));
+        System.out.println("solution2 : " + solution2(start, end, new HashSet<>(disc)));
         System.out.println("answer : " + answer(start, end, new HashSet<>(disc)));
+        System.out.println("\n");
 
         String start2 = "toon";
         String end2 = "plea";
         Set<String> disc2 = new HashSet<>(Arrays.asList("poon", "plee", "same", "poie", "plea", "plie", "poin"));
-        System.out.println("solution2 : " + solution(start2, end2, new HashSet<>(disc2)));
-        System.out.println("answer2 : " + answer(start2, end2, new HashSet<>(disc2)));
+        System.out.println("solution : " + solution(start2, end2, new HashSet<>(disc2)));
+        System.out.println("solution2 : " + solution2(start2, end2, new HashSet<>(disc2)));
+        System.out.println("answer : " + answer(start2, end2, new HashSet<>(disc2)));
+        System.out.println("\n");
 
         String start3 = "abcv";
         String end3 = "ebad";
         Set<String> disc3 = new HashSet<>(Arrays.asList("abcd", "ebad", "ebcd", "xyza"));
-        System.out.println("solution3 : " + solution(start3, end3, new HashSet<>(disc3)));
-        System.out.println("answer3 : " + answer(start3, end3, new HashSet<>(disc3)));
+        System.out.println("solution : " + solution(start3, end3, new HashSet<>(disc3)));
+        System.out.println("solution2 : " + solution2(start3, end3, new HashSet<>(disc3)));
+        System.out.println("answer : " + answer(start3, end3, new HashSet<>(disc3)));
+        System.out.println("\n");
 
         String start4 = "fool";
         String end4 = "sage";
         Set<String> disc4 = new HashSet<>(Arrays.asList("fool", "pool", "poll", "pole", "pale", "sale", "sage"));
-        System.out.println("solution4 : " + solution(start4, end4, new HashSet<>(disc4)));
-        System.out.println("answer4 : " + answer(start4, end4, new HashSet<>(disc4)));
+        System.out.println("solution : " + solution(start4, end4, new HashSet<>(disc4)));
+        System.out.println("solution2 : " + solution2(start4, end4, new HashSet<>(disc4)));
+        System.out.println("answer : " + answer(start4, end4, new HashSet<>(disc4)));
+        System.out.println("\n");
 
         String start5 = "der";
         String end5 = "dfs";
         Set<String> disc5 = new HashSet<>(Arrays.asList("des","der","dfr","dgt","dfs"));
-        System.out.println("solution5 : " + solution(start5, end5, new HashSet<>(disc5)));
-        System.out.println("answer5 : " + answer(start5, end5, new HashSet<>(disc5)));
+        System.out.println("solution : " + solution(start5, end5, new HashSet<>(disc5)));
+        System.out.println("solution2 : " + solution2(start5, end5, new HashSet<>(disc5)));
+        System.out.println("answer : " + answer(start5, end5, new HashSet<>(disc5)));
+        System.out.println("\n");
 
         String start6 = "gedk";
         String end6 = "geek";
         Set<String> disc6 = new HashSet<>(Arrays.asList("geek", "gefk"));
-        System.out.println("solution6 : " + solution(start6, end6, new HashSet<>(disc6)));
-        System.out.println("answer6 : " + answer(start6, end6, new HashSet<>(disc6)));
+        System.out.println("solution : " + solution(start6, end6, new HashSet<>(disc6)));
+        System.out.println("solution2 : " + solution2(start6, end6, new HashSet<>(disc6)));
+        System.out.println("answer : " + answer(start6, end6, new HashSet<>(disc6)));
+        System.out.println("\n");
     }
 
+    // recursive solution
     private static int solution(String beginWord, String endWord, Set<String> wordDict){
         List<String> steps = new ArrayList<>();
         steps.add(beginWord);
         return countSteps(beginWord, endWord, wordDict, steps);
+    }
+
+    // BFS solution > better
+    private static int solution2(String beginWord, String endWord, Set<String> wordDict){
+        Queue<String> queue = new LinkedList<>();
+        Queue<Integer> counts = new LinkedList<>();
+        counts.add(1);
+        queue.add(beginWord);
+
+        while(!queue.isEmpty()){
+            String workingWord = queue.remove();
+            int workingCount = counts.remove();
+            System.out.println(workingWord + " - workingCount : " + workingCount + ", Queue: " + queue);
+            if(isOneCharDiff(workingWord,endWord)){
+                return workingCount + 1;
+            }
+            for(String dict : new HashSet<>(wordDict)){
+                if(isOneCharDiff(workingWord, dict)){
+                    queue.add(dict);
+                    counts.add(workingCount + 1);
+                    wordDict.remove(dict);
+                }
+            }
+        }
+        return 0;
     }
 
     private static int  countSteps(String start, String end, Set<String> ladder, List<String> steps){
